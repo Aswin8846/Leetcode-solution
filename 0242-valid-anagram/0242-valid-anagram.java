6004ca460b3d5bac87678c183763d681c342e346
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-09-14 21:28:59 +0530

    Time: 3 ms (90.95%), Space: 43.4 MB (56.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,11 @@
 class Solution {
-
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        int[] store = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            store[s.charAt(i) - 'a']++;
-            store[t.charAt(i) - 'a']--;
-        }
-
-        for (int n : store) if (n != 0) return false;
-
-        return true;
+        char[] ch = s.toCharArray();
+        char[] c = t.toCharArray();
+        Arrays.sort(ch);
+        Arrays.sort(c);
+        return Arrays.equals(ch,c);
+        
+        
     }
 }
