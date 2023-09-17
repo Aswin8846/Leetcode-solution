fff457fc826b994df598eb362c864f0b5e32fe55
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-09-17 21:33:02 +0530

    Time: 205 ms (31.18%), Space: 45.3 MB (50.88%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] fairCandySwap(int[] A, int[] B) {
         int sumA = 0, sumB = 0;
-        Set<Integer> setA = new HashSet<>();
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 0; i < A.length; i++)
             sumA += A[i];
-            setA.add(A[i]);
-        }
         for (int i = 0; i < B.length; i++)
             sumB += B[i];
         int dif = (sumA - sumB) / 2;
-        for (int i = 0; i < B.length; i++) {
-            int targetA = B[i] + dif;
-            if (setA.contains(targetA))
-                return new int[]{targetA, B[i]};
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] - B[j] == dif)
+                    return new int[]{A[i], B[j]};
+            }
         }
         return null;
     }
