93a01e9304969a766e3e9c2e43a540c033841425
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-11-16 13:25:11 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (7.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -11,7 +11,8 @@ class Solution {
             return binarySearch(nums,target,0,pivot-1);
         }
         return binarySearch(nums,target,pivot+1,nums.length-1);
-    }
+    }   
+    
     int binarySearch(int[] nums,int target,int start,int end){
         while(start<=end){
             int mid = start+(end-start)/2;
@@ -27,21 +28,21 @@ class Solution {
         }
         return -1;
     }
+    
+    
+    
     int findPivot(int[] arr){
         int start = 0;
         int end = arr.length-1;
         while(start<=end){
             int mid = start + (end-start)/2;
-            if(end>mid && arr[mid]>arr[mid+1]){
+            if(mid<end && arr[mid]>arr[mid+1]){
                 return mid;
-            }
-            if(start<mid && arr[mid]<arr[mid-1]){
+            }if(mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
-            }
-            else if(arr[mid]<=arr[start]){
+            }if(arr[mid]<arr[start]){
                 end = mid-1;
-            }
-            else{
+            }else{
                 start = mid+1;
             }
         }
