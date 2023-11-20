103c7800e6276e52beff6fe7008b365e3403ecbe
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-11-20 18:01:17 +0530

    Time: 3 ms (96.97%), Space: 61.2 MB (66.48%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,16 +1,34 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int last[] = new int[128], res = 0;
-        for (int i = 0; i < garbage.length; ++i) {
-            res += garbage[i].length();
-            for (int j = 0; j < garbage[i].length(); ++j)
-                last[garbage[i].charAt(j)] = i;
+        int n = garbage.length;
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans += 3 * travel[i];
         }
-        for (int j = 1; j < travel.length; ++j)
-            travel[j] += travel[j - 1];
-        for (int c : "PGM".toCharArray())
-            if (last[c] > 0)
-                res += travel[last[c] - 1];
-        return res;
+        for (String s : garbage) {
+            ans += s.length();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("G")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("P")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("M")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        return ans;
     }
 }
