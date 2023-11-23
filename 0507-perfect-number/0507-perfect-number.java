ded001ea7727d84364de6299a746d1179bbffdce
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-11-23 09:58:31 +0530

    Time: 2086 ms (40.11%), Space: 39 MB (60.71%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i=1;i<num;i++){
-            if(num%i==0){
-                sum+=i;
+        if (num <= 1) {
+            return false; // Perfect numbers are greater than 1
+        }
+
+        int sum = 1; // Start with 1, as it is a divisor for every number
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
             }
         }
-        if(sum==num){
-            return true;
-        }
-         return false;
+
+        return sum == num;
     }
-   
 }
