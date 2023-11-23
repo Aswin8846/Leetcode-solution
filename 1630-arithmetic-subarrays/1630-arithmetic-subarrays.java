255e7db44f1cf56d6a7fcb5befc23831afed5b7b
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2023-11-23 09:17:17 +0530

    Time: 16 ms (94.51%), Space: 44.8 MB (9.62%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,41 +1,28 @@
 class Solution {
- public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> result = new ArrayList<>(l.length);
-        for (int i = 0; i < l.length; i++) {
-           result.add(isArithmetic(nums, l[i], r[i]));
-        }
-        return result;
-   }
-
-
-   public boolean isArithmetic(int [] nums, int l, int r){
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = l; i <= r; i++) {
-            max = Math.max(nums[i],max);   
-            min = Math.min(nums[i],min);        
-        }
-        int len = r -l +1;
-        if((max-min)%(len-1) !=0)               
-            return false;
-
-        int diff = (max-min)/(len-1);
-        if(diff == 0)
-            return true;
-
-        boolean [] visited = new boolean[len];
-
-        for (int i = l; i <= r; i++) {
-        int val = nums[i];
-            if((val - min)% diff != 0)          
+    public Boolean check(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
                 return false;
-            else {
-                int pos = (val - min) / diff;
-                if(visited[pos])              
-                    return false;
-                visited[pos] = true;        
             }
         }
+        
         return true;
-    } 
+    }
+    
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> ans = new ArrayList();
+        for (int i = 0; i < l.length; i++) {
+            int[] arr = new int[r[i] - l[i] + 1];
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = nums[l[i] + j];
+            }
+            
+            ans.add(check(arr));
+        }
+
+        return ans;
+    }
 }
