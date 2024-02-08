f4d30c30567887525c9142480370764aaa14a644
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2024-02-08 21:36:35 +0530

    Time: 22 ms (78.52%), Space: 56.1 MB (94.81%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -5,10 +5,9 @@ class Solution {
        for(int num:nums) hs.add(num);
        int longest =1;
        for(int num: nums ){
-           //check if the num is the start of a sequence by checking if left exists
-           if(!hs.contains(num-1)){ // start of a sequence
+           if(!hs.contains(num-1)){
                 int count =1;
-                while(hs.contains(num + 1)){ // check if hs contains next no.
+                while(hs.contains(num + 1)){ 
                     num++;
                     count++;
                 }
