05235fd8a6def3af727203216be9bf5736bf75ae
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2024-03-05 22:13:53 +0530

    Time: 8 ms (97.95%), Space: 48.4 MB (64.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,65 +1,32 @@
 class Solution {
-
-    /**
+     /**
      * Time Complexity: O(n)
      * Space Complexity: O(n)
      */
-    public int[] topKFrequent(int[] numbers, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int number : numbers) map.put(
-            number,
-            map.getOrDefault(number, 0) + 1
-        );
-
-        int size = map.size();
-        int[] keys = new int[size];
-        int i = 0;
-        for (int key : map.keySet()) keys[i++] = key;
-
-        select(keys, map, 0, size - 1, size - k);
-        return Arrays.copyOfRange(keys, size - k, size);
-    }
-
-    // Modified implementation of Hoare's selection algorithm:
-
-    private void select(
-        int[] keys,
-        Map<Integer, Integer> map,
-        int left,
-        int right,
-        int kSmallest
-    ) {
-        while (left != right) {
-            int pivot = partition(keys, map, left, right, (left + right) / 2);
-
-            if (kSmallest == pivot) return;
-
-            if (kSmallest < pivot) right = pivot - 1; else left = pivot + 1;
+     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];                
+        
+        for (int num : nums)
+            count.merge(num, 1, Integer::sum);
+        
+        for (int key : count.keySet()){
+            int freq = count.get(key);
+            if (bucket[freq] == null)
+                bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
         }
+        
+        int index = 0;
+        int[] res = new int[k];
+        for (int i = nums.length; i >= 0; i--)
+            if (bucket[i] != null)
+                for (int val : bucket[i]){
+                    res[index++] = val;
+                    if(index == k)
+                        return res;
+                }
+        return res;
     }
-
-    private int partition(
-        int[] keys,
-        Map<Integer, Integer> map,
-        int left,
-        int right,
-        int pivot
-    ) {
-        int pivotValue = map.get(keys[pivot]);
-        swap(keys, pivot, right);
-        int index = left;
-
-        for (int i = left; i <= right; i++) if (map.get(keys[i]) < pivotValue) {
-            swap(keys, i, index);
-            index++;
-        }
-        swap(keys, right, index);
-        return index;
-    }
-
-    private void swap(int[] array, int i1, int i2) {
-        int temp = array[i1];
-        array[i1] = array[i2];
-        array[i2] = temp;
-    }
+       
 }
