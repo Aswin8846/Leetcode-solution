86206729e77bddb6ccf4afd33b0fd3c3b44cd208
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2024-03-05 22:12:47 +0530

    Time: 14 ms (63.75%), Space: 48.4 MB (64.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,25 @@
-import java.util.*;
-
 class Solution {
+
+    /**
+     * Time Complexity: O(nlog(k))
+     * Space Complexity: O(n)
+     */
     public int[] topKFrequent(int[] nums, int k) {
-        // Create a map to store the frequency of each number
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-        }
-        
-        // Create a priority queue to store elements based on their frequency
+        int[] arr = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
-            (a, b) -> b.getValue() - a.getValue()); // Max heap based on frequency
-        
-        // Add all entries from the map to the priority queue
-        pq.addAll(freqMap.entrySet());
-        
-        // Extract top k frequent elements from the priority queue
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = pq.poll().getKey();
+                (a, b) ->
+            a.getValue() - b.getValue()
+        );
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+            pq.add(it);
+            if (pq.size() > k) pq.poll();
         }
-        
-        return result;
+        int i = k;
+        while (!pq.isEmpty()) {
+            arr[--i] = pq.poll().getKey();
+        }
+        return arr;
     }
 }
