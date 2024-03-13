11ac438f9c9379af059d809bf38dda5306b0f566
Aswin8846 <119944908+Aswin8846@users.noreply.github.com>
2024-03-13 18:12:28 +0530

    Time: 1 ms (100.00%), Space: 53.9 MB (33.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,26 +1,34 @@
+//Just store the left and right product (Try doing this with extra space first)
+//This one is constant space because we are returning the array we created
+//In first pass calculate the left product except self and in second calculate the right
+
 class Solution {
+
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-        
-        // Initialize the answer array with all 1s
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = 1;
-        }
-        
-        int leftProduct = 1;
-        // Calculate products of elements to the left of current element
+        int[] arr = new int[nums.length];
+        int right = 1, left = 1;
         for (int i = 0; i < nums.length; i++) {
-            answer[i] *= leftProduct;
-            leftProduct *= nums[i];
+            arr[i] = left;
+            left *= nums[i];
         }
-        
-        int rightProduct = 1;
-        // Calculate products of elements to the right of current element and multiply with answer
         for (int i = nums.length - 1; i >= 0; i--) {
-            answer[i] *= rightProduct;
-            rightProduct *= nums[i];
+            arr[i] *= right;
+            right *= nums[i];
         }
-        
-        return answer;
+        return arr;
+    }
+
+    public int[] productExceptSelfNumsAsPrefix(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) output[i + 1] =
+            output[i] * nums[i];
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            output[i] = nums[i + 1] * output[i];
+            nums[i] = nums[i] * nums[i + 1];
+        }
+        return output;
     }
 }
